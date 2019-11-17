ac4f2025a4e2794d3edaaf9955a99575fc9386cd
Anthony <anthony@Apples-MacBook-Pro.local>
2019-11-17 18:18:59 -0500

    prioritization of attacking moves

--- engine.py
+++ engine.py
@@ -3,10 +3,12 @@ File for chess engine class
 Includes functions for all decision making processes
 '''
 import random
+import chess
 
 class Engine:
     def __init__(self):
-        pass
+        self.color = 1
+        self.opcolor = 0
 
     def choose_move(self, board):
         '''
@@ -15,4 +17,18 @@ class Engine:
         Make random move
         '''
         moves = [move for move in board.legal_moves]
+        attacks = self.attacking_moves(board, moves)
+
+        if attacks:
+            return random.choice(attacks)
         return random.choice(moves)
+
+    def attacking_moves(self, board, moves):
+        '''
+        Returns list of attacking moves
+        '''
+        out = []
+        for m in moves:
+            if 'x' in board.san(m):
+                out.append(m)
+        return out
